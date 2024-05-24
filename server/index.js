e86f69f0272a6fd1dde436c665a56ecc2b0680ab
Leif Taylor <ltaylor@connectbase.com>
2024-05-24 01:51:42 -0400

    Remove Redis for now

--- server/index.js
+++ server/index.js
@@ -3,10 +3,8 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const cors = require('cors');
-const Redis = require('ioredis');
 const { initTables } = require('./db/database');
 const { populateTables } = require('./db/populateTables');
-const { serverTick } = require('./services/server/tick');
 const authHandler = require('./handlers/api/authHandler');
 const characterHandler = require('./handlers/api/characterHandler');
 const shopHandler  = require('./handlers/api/shopHandler');
@@ -48,27 +46,8 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 
-const redis = new Redis();
-redis.subscribe('serverTickChannel', (err, count) => {
-  if (err) {
-    console.error('Failed to subscribe: %s', err.message);
-  } else {
-    console.log(`Subscribed successfully! This client is currently subscribed to ${count} channels.`);
-  }
-});
-
-redis.on('message', (channel, message) => {
-  if (channel === 'serverTickChannel') {
-    console.log('Broadcasting serverTick to clients:', message);
-    io.emit('serverTick', message);
-  }
-});
-
 server.listen(PORT, async () => {
   await initTables();
   await populateTables(); // Call populateTables after initTables
   console.log(`Server is running on port ${PORT}`);
-
-  // Start the server tick loop
-  // serverTick();
 });
