f3abd0ea96e4be16968714782992e4e9a49269b9
Leif Taylor <ltaylor@connectbase.com>
2024-05-25 21:45:07 -0400

    Add future encounters

--- server/db/data/encounterTemplates.js
+++ server/db/data/encounterTemplates.js
@@ -17,6 +17,35 @@ const encounterTemplates = [
       ]),
       is_boss: true
     }
+    // 1 Orcish Warrior, 2 Orcish Grunts
+    // 1 Orcish Warrior, 1 Orcish Hunter, 1 Orcish Mystic
+    // 1 Orcish Mage, 2 Orcish Warriors
+    // 1 Orcish Mystic, 1 Bugbear
+    // 1 Ogre Mystic, 1 Orcish Warrior
+    // 2 Dire Wolves
+    // 1 Dire Wolf, 2 Orcish Warriors
+    // 2 Bugbears
+    // 2 Werebears
+    // 3 Sneering Imps
+    // 1 Owlbear Horror (is_boss)
+    // 2 Dire Porcupines
+    // 1 Giant Beetle, 1 Giant Wasp
+    // 2 Giant Wasps
+    // 2 Winged Garrotters
+    // 1 Giant Beetle, 1 Giant Wasp, 1 Winged Garrotter
+    // 1 Giant Spider, 1 Giant Wasp
+    // 2 Giant Spiders
+    // 1 Orcish Mage, 2 Giant Spiders
+    // 1 Lumbering Centipede, 1 Giant Spider
+    // 1 Lumbering Centipede, 1 Giant Spider, 1 Giant Wasp
+    // 2 Flying Millipedes
+    // 1 Demonic Howler (is_boss)
+    // 1 Demonic Serpent (is_boss)
+    // 1 Infested Treant (is_boss)
+    // 3 Treant Youth
+    // 2 Treant Youth, 1 Fiendish Shrub
+    // 3 Fiendish Shrubs
+    // 2 Forest Wights
   ];
   
 module.exports = encounterTemplates;
